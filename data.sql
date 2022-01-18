2a6354c99194ee24c9bec03fac9e3f47ef642da0
ermanyeltekin <97963368+ermanyeltekin@users.noreply.github.com>
2022-01-18 22:25:59 +0300

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: localhost
+-- Üretim Zamanı: 18 Oca 2022, 18:03:13
+-- Sunucu sürümü: 8.0.17
+-- PHP Sürümü: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `data`
+--
+
+CREATE TABLE `data` (
+  `isim` text NOT NULL,
+  `soyisim` text NOT NULL,
+  `eposta` varchar(50) NOT NULL,
+  `sifre` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
